aa6ab6a5e01ae496925f82df51227f2ad53e2fa0
Johan Aanesen <johanaan@stud.ntnu.no>
2017-11-01 19:05:27 +0100

    removed some stuff

--- cmd/oblig2/main.go
+++ cmd/oblig2/main.go
@@ -6,17 +6,9 @@ import (
 )
 
 //Discord webhook
-//var url = "https://discordapp.com/api/webhooks/370393359900082200/_RASdjfNlTsFm9QMprDIFukfV05u7_vfN8nBjgoJ7y0_D_JmLXYdoWVbY8guoCkbOAVx"
-//Discord webhook
 //var url = "https://discordapp.com/api/webhooks/371707670832349187/dPg6uA7eJL1K0wPxtfyde1ZQu_6LoC_O_SOqrQJ5b_VqcxpfsnGHE4TYKrNz95sAXW3o"
-//Slack webhook
-//var url = "https://hooks.slack.com/services/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"
 
 func main() {
-	////////////NEED TO FIGURE THIS ONE OUT////////////
-	/*	for range time.NewTicker(24 * time.Second).C {
-		updateCurrencies()
-	}*/
 
 	http.HandleFunc("/", funcs.HandleMain)
 	http.HandleFunc("/latest", funcs.HandleLatest)
