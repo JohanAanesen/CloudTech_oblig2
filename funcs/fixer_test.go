0b1e95fbdbfa11f0331e2e589dbf920299cf49fd
Johan Aanesen <johanaan@stud.ntnu.no>
2017-11-03 16:00:15 +0100

    go fmt

--- funcs/fixer_test.go
+++ funcs/fixer_test.go
@@ -13,10 +13,9 @@ func TestGetFixer(t *testing.T) {
 	hour, _, _ := time.Now().Clock()
 	timeTest := time.Now()
 	if hour < 16 {
-		timeTest = timeTest.AddDate(0,0,-1)
+		timeTest = timeTest.AddDate(0, 0, -1)
 	}
 	timeTestString := timeTest.Format("2006-01-02")
-
 
 	db := DatabaseCon()
 	defer db.Close()
